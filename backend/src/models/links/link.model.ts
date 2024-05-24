55f20c33856bcadd65bcc194f8ff56f6238a496a
BananaBard <brunogonzalez.dev@gmail.com>
2024-05-23 23:17:15 -0300

    Merged link schema and model Merged the 2 scripts to 1.

--- backend/src/models/links/link.model.ts
+++ backend/src/models/links/link.model.ts
@@ -1,6 +1,17 @@
-import { model } from "mongoose";
-import linkSchema from "./link.schema";
+import { Schema, model } from "mongoose";
 
-const LinkModel = model('Link', linkSchema);
+interface Link extends Document {
+    link_key: string;
+    original_link: string;
+    createdAt: Date;
+}
 
-export default LinkModel
+const linkSchema = new Schema({
+    link_key: { type: String, required: true },
+    original_link: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now() }
+});
+
+const LinkModel = model<Link>('Link', linkSchema);
+
+export default LinkModel;
