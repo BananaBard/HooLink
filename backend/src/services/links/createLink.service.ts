66d13dcd249dba763fe27bfd3e7b182523d80379
BananaBard <brunogonzalez.dev@gmail.com>
2024-05-27 02:40:40 -0300

    Updated creat link service Now it uses supabase, also the generated id for shortened links is now 4 characters long

--- backend/src/services/links/createLink.service.ts
+++ backend/src/services/links/createLink.service.ts
@@ -1,36 +1,43 @@
-import LinkModel from "../../models/links/link.model";
+import { supabase } from "../../infra/supabase";
 const xid = require('xid-js');
 
-const createLinkService = async (originalURL: string) => {
+
+const createLinkService = async (originalURL: string, expTimeInMinutes: number) => {
+    const base_url = process.env.DEV_URL
     
-    const keyLength: number = 8;
     let checkForDuplicatedKeys: Boolean = true;
-    let newKey: string = xid.next();
+    let newKey: string = xid.next().slice(-4);
 
     while(checkForDuplicatedKeys) {
-        let linkFoundWithKey;
+        let supabaseRes;
         try {
-            linkFoundWithKey = await LinkModel.findOne({link_key: newKey});
+            supabaseRes = await supabase.from('links').select().eq('shortened_url', newKey);
         } catch(error) {
-            throw new Error('MongoDB Error.');
+            throw new Error('Supabase Error.');
         }
 
-        if (!linkFoundWithKey) {
-            checkForDuplicatedKeys = false;
+        if (supabaseRes.status === 200) {
+            newKey = xid.next().slice(-4);
         } else {
-            newKey = xid.next();
+            checkForDuplicatedKeys = false;
         }
-        
+
         try {
-            const newLink = await LinkModel.create({
-                original_link: originalURL,
-                link_key: newKey,
+            const now = new Date()
+            const res = await supabase.from('links').insert({
+                original_url: originalURL,
+                shortened_url: base_url + newKey,
+                expiresAt: new Date(now.getTime() + expTimeInMinutes * 60 * 60 * 1000),
+                /* add creator when login is setted */
             });
 
-            return newLink;
+            if (res.status === 201) {
+                const newLink = await supabase.from('links').select().eq('shortened_url', base_url+newKey)
+                return newLink.data?.[0];
+            }
 
         } catch(error) {
-            throw new Error('MongoDB Error.');
+            throw new Error('Supabase Error.');
         }
     }
 }
