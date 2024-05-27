1c2d154e26f790164c2b7eb1321eaa07b8245516
BananaBard <brunogonzalez.dev@gmail.com>
2024-05-27 02:38:41 -0300

    Created supabase client

---
+++ backend/src/infra/supabase.ts
@@ -0,0 +1,4 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient('https://taqstfcchfkiuzljzubl.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhcXN0ZmNjaGZraXV6bGp6dWJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY3NzY4NzIsImV4cCI6MjAzMjM1Mjg3Mn0.MvQdjEk2_lI0ooJdeEm_wTu_lOk17PtlqQ9kXgZk7Aw');
+
