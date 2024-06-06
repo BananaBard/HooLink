41993d980484b39e7e3417efec57a69860271c98
BananaBard <brunogonzalez.dev@gmail.com>
2024-06-06 18:59:41 -0300

    V1

--- backend/src/types.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Link {
-    shortened_url: string,
-    original_url: string,
-    createdAt: Date,
-    expiresAt: Date,
-}
--- frontend/src/types.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Link {
-    original_url: string,
-    shortened_url?: string,
-    createdAt: Date,
-    expiresAt: Date,
-    creator?: string
-}
