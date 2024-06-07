1b65146a11e376d2b03e8f004c3e02fbe22c3fdb
BananaBard <brunogonzalez.dev@gmail.com>
2024-06-06 23:35:12 -0300

    Added nav Added react router dom navlink and hover style

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,32 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import Pages from "../../utils/pages.utils";
+
+const hoverActive = 'hover:underline hover:decoration-purple-500 hover:decoration-2 hover:underline-offset-4'
 
 export default function Header() {
   const { user, signOut } = useAuth();
   return (
     <header className="text-white bg-neutral-900 border-b border-neutral-600">
-      <div
-        className="py-4 h-16 flex justify-between items-center max-w-7xl mx-auto"
-      >
+      <div className="py-4 px-14 lg:px-0 h-16 flex justify-between items-center max-w-7xl mx-auto">
         <h2 className="text-bold text-2xl">
-          <Link to={"/"}>HooLink</Link>
+          <Link to={Pages.home}>HooLink</Link>
         </h2>
-        {user ? (
-          <button
-            type="button"
-            onClick={signOut}
-            className="py-2 px-6 rounded-lg border-2 border-neutral-500"
-          >
-            Sign out
-          </button>
-        ) : (
-          <Link
-            to="/login"
-            className="py-2 px-6 rounded-lg border-2 border-neutral-500"
-          >
-            Login
-          </Link>
-        )}
+        <nav className="flex gap-8 justify-between items-center">
+          <NavLink className={hoverActive} to={Pages.home}>Home</NavLink>
+          {user && <NavLink className={hoverActive} to={Pages.dashboard}>Dashboard</NavLink>}
+
+          {user ? (
+            <button
+              type="button"
+              onClick={signOut}
+              className="py-2 px-6 rounded-lg border-2 border-neutral-500"
+            >
+              Sign out
+            </button>
+          ) : (
+            <Link
+              to="/login"
+              className="py-2 px-6 rounded-lg border-2 border-neutral-500"
+            >
+              Login
+            </Link>
+          )}
+        </nav>
       </div>
     </header>
   );
