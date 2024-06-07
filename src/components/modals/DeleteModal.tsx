f4ae435a11e897e4567d933e502c362a472d1bae
BananaBard <brunogonzalez.dev@gmail.com>
2024-06-06 23:14:56 -0300

    Fixed UI

--- src/components/modals/DeleteModal.tsx
+++ src/components/modals/DeleteModal.tsx
@@ -7,14 +7,14 @@ interface DeleteModal {
 
 const DeleteModal = ({hideModal, handleDeleteLink}: DeleteModal) => {
     return (
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4">
         <div>
             <p>Deleting this links will remove it permanently.</p>
             <p>This action can not be undone.</p>
         </div>
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-row gap-8 justify-between">
         <button className="py-2 px-6 rounded-lg border-2 border-neutral-500" onClick={hideModal}>Cancel</button>
-        <Button className="bg-red-600" onClick={handleDeleteLink}>Delete</Button>
+        <Button className="bg-red-800/60 border border-red-800 hover:border-transparent hover:bg-red-800 transition-all" onClick={handleDeleteLink}>Delete</Button>
         </div>
         </div>
     )
