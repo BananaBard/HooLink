8cf4e845953a235589eceae93dc656abfe47cbe7
BananaBard <brunogonzalez.dev@gmail.com>
2024-06-10 20:44:51 -0300

    env set

--- src/infra/services/createLink.service.ts
+++ src/infra/services/createLink.service.ts
@@ -1,17 +1,23 @@
-import { supabase } from "../supabase"
-import { v4 as uuidv4 } from 'uuid';
+import { supabase } from "../supabase";
+import { v4 as uuidv4 } from "uuid";
 import { CreateLink } from "../../types";
-const base_url: string = import.meta.env.VITE_BASE_URL
+const base_url: string = import.meta.env.VITE_BASE_URL;
 
-const createLinkService = async({originalURL, expTimeInMinutes, creator, description, tags}: CreateLink) => {
-    let newKey: string = uuidv4().slice(-5)
+const createLinkService = async ({
+  originalURL,
+  expTimeInMinutes,
+  creator,
+  description,
+  tags,
+}: CreateLink) => {
+  let newKey: string = uuidv4().slice(-5);
 
-    const existShortURL = await supabase
+  const existShortURL = await supabase
     .from("links")
     .select()
     .eq("shortened_url", base_url + newKey);
   if (existShortURL.data) {
-    newKey = uuidv4().slice(-5)
+    newKey = uuidv4().slice(-5);
   }
   try {
     const now = new Date();
@@ -21,7 +27,7 @@ const createLinkService = async({originalURL, expTimeInMinutes, creator, descrip
       expiresAt: new Date(now.getTime() + expTimeInMinutes * 60 * 60 * 1000),
       description: description,
       creator: creator,
-      tags: tags
+      tags: tags,
     });
     if (res.status === 201) {
       const newLink = await supabase
@@ -33,6 +39,6 @@ const createLinkService = async({originalURL, expTimeInMinutes, creator, descrip
   } catch (error) {
     throw new Error("Could not create link, try again later...");
   }
-}
+};
 
-export default createLinkService
+export default createLinkService;
