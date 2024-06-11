7e87b98526acc80d6cd6c3eea038dc42f08d4f54
BananaBard <brunogonzalez.dev@gmail.com>
2024-06-10 21:27:21 -0300

    Added redirect to notfound page

--- src/pages/Redirect.tsx
+++ src/pages/Redirect.tsx
@@ -1,13 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Loader from "../components/utils/Loader";
 import { useMutation } from "@tanstack/react-query";
 import redirectLinkService from "../queries/redirectLink";
 import { useEffect } from "react";
+import Pages from "../utils/pages.utils";
 
 export default function RedirectPage() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const { mutate: redirectLink, isPending } = useMutation({
     mutationFn: redirectLinkService,
+    onError: () => {
+      navigate(Pages.notFound);
+    },
   });
 
   useEffect(() => {
