6d4337fa15fc5f39985424d46fd0d563409ce13d
BananaBard <brunogonzalez.dev@gmail.com>
2024-06-15 00:06:46 -0300

    Icons fix Fixed camelCasing in svg and added Refresh icon

--- src/components/icons/RefreshIcon.tsx
+++ src/components/icons/RefreshIcon.tsx
@@ -1,8 +1,8 @@
-export default function SortDesLetterIcon() {
+export default function RefreshIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="icon icon-tabler icon-tabler-sort-descending-letters"
+      className="icon icon-tabler icon-tabler-refresh"
       width="24"
       height="24"
       viewBox="0 0 24 24"
@@ -13,10 +13,8 @@ export default function SortDesLetterIcon() {
       strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M15 21v-5c0 -1.38 .62 -2 2 -2s2 .62 2 2v5m0 -3h-4" />
-      <path d="M19 10h-4l4 -7h-4" />
-      <path d="M4 15l3 3l3 -3" />
-      <path d="M7 6v12" />
+      <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />
+      <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
     </svg>
   );
 }
