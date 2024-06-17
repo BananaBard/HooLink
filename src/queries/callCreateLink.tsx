a8d02544f359c5729726078bd3d5e805477aeb16
BananaBard <brunogonzalez.dev@gmail.com>
2024-06-17 06:00:33 -0300

    Formated

--- src/queries/callCreateLink.tsx
+++ src/queries/callCreateLink.tsx
@@ -2,23 +2,33 @@ import { toast } from "sonner";
 import { type Link, type CreateLink } from "../types";
 import createLinkService from "../infra/services/createLink.service";
 
+const callCreateLink = async ({
+  originalURL,
+  expTimeInMinutes,
+  creator,
+  description,
+  tags,
+}: CreateLink): Promise<Link> => {
+  try {
+    new URL(originalURL);
+  } catch (error) {
+    toast.error("Use a valid URL.");
+    throw new Error("Invalid URL.");
+  }
+  try {
+    const res = await createLinkService({
+      originalURL,
+      expTimeInMinutes,
+      creator,
+      description,
+      tags,
+    });
+    toast.success("Link created!");
+    return res;
+  } catch (error) {
+    toast.error("Your link could not been created.");
+    throw new Error("Something went wrong");
+  }
+};
 
-const callCreateLink = async ({originalURL, expTimeInMinutes, creator, description, tags}:CreateLink) :Promise<Link> => {
-    try {
-        new URL(originalURL)
-    } catch(error) {
-        toast.error('Use a valid URL.');
-        throw new Error('Invalid URL.')
-    }
-    try {
-        const res = await createLinkService({originalURL, expTimeInMinutes, creator, description, tags})
-        toast.success('Link created!')
-        return res
-
-    } catch(error) {
-        toast.error('Your link could not been created.');
-        throw new Error('Something went wrong');
-    }
-}
-
-export default callCreateLink
+export default callCreateLink;
