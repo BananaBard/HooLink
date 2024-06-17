147826c0c28f286660add42a1e6568a9bbd94b7f
BananaBard <brunogonzalez.dev@gmail.com>
2024-06-16 21:32:51 -0300

    Separated redirect link to services Moved it to where it should be

---
+++ src/infra/services/redirectLink.service.ts
@@ -0,0 +1,46 @@
+import { supabase } from "../supabase";
+
+const base_url = import.meta.env.VITE_BASE_URL;
+
+const redirectLinkService = async (id: string): Promise<void> => {
+  const fullUrl = `${base_url}${id}`;
+  try {
+    const res = await supabase
+      .from("links")
+      .select()
+      .eq("shortened_url", fullUrl)
+      .single();
+
+    if (res.status !== 200) {
+      throw new Error(String(res.error));
+    }
+
+    let expDate = new Date(res.data.expiresAt);
+    let now = new Date();
+    let linkId = res.data.id;
+
+    if (now > expDate) {
+      let deleteRes = await supabase.from("links").delete().eq("id", linkId);
+
+      if (deleteRes.status === 204) {
+        throw new Error("The link expired");
+      }
+    }
+
+    await incrementClickCount(linkId)
+    window.location.replace(res.data.original_url)
+  } catch (err) {
+    throw err;
+  }
+};
+
+async function incrementClickCount(linkId: string) {
+  const { error } = await supabase.rpc("increment_click_count", {
+    link_id: linkId,
+  });
+  if (error) {
+    console.error("Error incrementing click count:", error);
+  }
+}
+
+export default redirectLinkService;
