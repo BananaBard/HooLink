bcd2ea2667716a5c89419c75706d4e4b8c02ca0d
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-25 23:14:20 -0300

    fix: fixed styling

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,18 +1,23 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import Pages from "../../utils/pages.utils";
+import { Button } from "../buttons/Button";
 
-const hoverActive = 'hover:underline hover:decoration-purple-500 hover:decoration-2 hover:underline-offset-4 text-lg'
+const hoverActive = 'hover:underline hover:decoration-purple-500 hover:decoration-2 hover:underline-offset-4 md:text-lg'
 
 export default function Header() {
   const { user, signOut } = useAuth();
+  const navigate = useNavigate();
   return (
     <header className="text-white bg-neutral-900 border-b border-neutral-600">
-      <div className="py-4 px-4 md:px-14 lg:px-12 xl:px-0 md:h-16 flex flex-col gap-4 md:flex-row justify-between md:items-center max-w-7xl mx-auto">
-        <h2 className="text-bold text-3xl">
+      <div className="py-4 px-4 md:px-14 lg:px-12 xl:px-0 md:h-16 flex gap-4 md:flex-row justify-between items-center max-w-7xl mx-auto">
+        <h2 className="text-bold md:hidden text-xl">
           <Link to={Pages.home}>HooLink</Link>
         </h2>
-        <nav className="flex gap-8 justify-between items-center">
+        <h2 className="hidden md:block text-bold text-3xl">
+          <Link to={Pages.home}>HooLink</Link>
+        </h2>
+        <nav className="flex gap-4 md:gap-8 justify-between items-center">
           <NavLink className={hoverActive} to={Pages.home}>Home</NavLink>
           {user && <NavLink className={hoverActive} to={Pages.dashboard}>Dashboard</NavLink>}
 
@@ -20,17 +25,12 @@ export default function Header() {
             <button
               type="button"
               onClick={signOut}
-              className="py-2 px-6 rounded-lg border-2 border-neutral-500"
+              className="inline-flex justify-center items-center py-2 px-6 rounded-lg border-2 border-neutral-500"
             >
               Log out
             </button>
           ) : (
-            <Link
-              to="/login"
-              className="py-2 px-6 rounded-lg border-2 border-neutral-500"
-            >
-              Login
-            </Link>
+            <Button className="hidden sm:block" onClick={() => navigate('/login')}>Login</Button>
           )}
         </nav>
       </div>
