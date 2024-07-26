777b5c86c890552f70701345eefae4dee82e3fa0
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-25 23:14:02 -0300

    fix: add children

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx'
 
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement>
 
-export const Button = ({className, ...rest}: Props) => {
+export const Button = ({className, children, ...rest }: Props) => {
     return <button className={clsx(`bg-purple-600 py-3 px-2 rounded-md w-[128px] font-medium
     hover:transition-all hover:scale-105
     outline-none ring-teal-600 ring-offset-2
@@ -11,5 +11,5 @@ export const Button = ({className, ...rest}: Props) => {
     focus-visible:ring-2 
     disabled:bg-slate-500 disabled:text-gray-300 disabled:cursor-not-allowed disabled:shadow-none
     disabled:scale-100
-    `, className)}{...rest}/>
+    `, className)}{...rest}>{children}</button>
 }
